4a206eff50da9b069d099187d6823001c2ef45e3
patilMohitDD <70277626+patilMohitDD@users.noreply.github.com>
2023-06-19 16:39:04 -0700

    Summary
    - Added testing file for Sudoku
    - updated >readme

---
+++ test_sudoku.py
@@ -0,0 +1,8 @@
+from main import square_area
+
+# def testMatrixLength(martix, expected_length) -> int:
+#     assert len(matrix) == expected_length
+
+def testArea():
+    assert square_area(5) == 25
+    
